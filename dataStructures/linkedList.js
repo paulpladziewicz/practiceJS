d38973a85f463dc08a389024071ff2664c5c5f71
paulpladziewicz <ppladziewicz@gmail.com>
2022-05-24 21:03:59 -0400

    added common methods to linked list

--- dataStructures/linkedList.js
+++ dataStructures/linkedList.js
@@ -1,24 +1,84 @@
+/*
+    Common Linked List Methods
+    appendToTail()
+    size()
+    clear()
+    getLast()
+    getFirst()
+ */
+
 class Node {
     next = null;
 
     constructor(data) {
         this.data = data;
     }
+}
+
+// Singly
+class LinkedList {
+    constructor(head = null) {
+        this.head = head;
+    }
 
     appendToTail(data) {
         let end = new Node(data);
-        let n = this;
 
-        while (n.next !== null) {
-            n = n.next;
+        if (this.head === null) {
+            return this.head = end;
         }
 
-        n.next = end;
+        let node = this.head;
+
+        while (node.next !== null) {
+            node = node.next;
+        }
+
+        node.next = end;
+    }
+
+    size() {
+        if (this.head === null) return 0;
+
+        let node = this.head;
+
+        let count = 1;
+
+        while (n.next !== null) {
+            count++;
+            node = node.next;
+        }
+
+        return count;
+    }
+
+    clear() {
+        this.head = null;
+    }
+
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        if (this.head === null) return null;
+
+        if (this.head.next === null) return this.head;
+
+        let node = this.head;
+
+        while (node.next !== null) {
+            node = node.next;
+        }
+
+        return node;
     }
 }
 
 let a = new Node(1);
-a.appendToTail(2)
-a.appendToTail(3)
+let b = new LinkedList(a)
 
-console.log(a);
+b.appendToTail(2);
+b.appendToTail(3);
+
+console.log(b.getLast())
