30aeeb6e607e9c81f63989254fb1864cd1f86d18
paulpladziewicz <ppladziewicz@gmail.com>
2022-06-05 07:07:09 -0400

    basic binaryTree complete

---
+++ dataStructures/binaryTree.js
@@ -0,0 +1,61 @@
+export class Node {
+    constructor(value) {
+        this.value = value;
+        this.right = null;
+        this.left = null;
+    }
+}
+
+export class BinaryTree
+{
+    constructor()
+    {
+        this.root = null;
+    }
+
+    insert(data) {
+        let newNode = new Node(data);
+
+        if (this.root === null) {
+            this.root = newNode;
+            return;
+        }
+
+        this.insertNode(this.root, newNode);
+    }
+
+    insertNode(node, newNode) {
+        if (newNode.value < node.value) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+}
+
+const BST = new BinaryTree();
+BST.insert(15);
+BST.insert(25);
+BST.insert(10);
+BST.insert(7);
+BST.insert(22);
+BST.insert(17);
+BST.insert(13);
+BST.insert(5);
+BST.insert(9);
+BST.insert(27);
+//          15
+//         /  \
+//        10   25
+//       / \   / \
+//      7  13 22  27
+//     / \    /
+//    5   9  17
